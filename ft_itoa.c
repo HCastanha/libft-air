6b68db6ad6164f62c56fbee2f1182a1bb066beb5
Herbert Castanha <hcastanha@Herberts-MacBook-Air.local>
2020-05-06 18:46:50 -0300

    itoa mods

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: hcastanh <hcastanh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 13:38:49 by hcastanh          #+#    #+#             */
-/*   Updated: 2020/05/02 15:32:09 by hcastanh         ###   ########.fr       */
+/*   Updated: 2020/05/06 18:45:55 by hcastanh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,15 @@ char *ft_itoa(int n)
 	int i = 0;
 	int n_cpy;
 
-	if (n == -2147483648)
-	{
-		n_cpy = -147483648;
-		i++;
-	}
+	if ((long)n == -2147483648)
+		return((char *)"-2147483648");
 	n_cpy = n;
 	while (n_cpy)
 	{
 		n_cpy /= 10;
 		i++;
 	}
-	if (n < 0 && n != -2147483648)
+	if (n < 0)
 	{
 		n_cpy = -n;
 		i++;
@@ -46,8 +43,6 @@ s = malloc(sizeof(char) * (i + 1));
 		s[i] = (n_cpy % 10) + '0';
 		n_cpy /= 10;
 	}
-	if (n == -2147483648)
-		s[1] = '2';
 	if (n < 0)
 		s[0] = '-';
 	if (n == 0)

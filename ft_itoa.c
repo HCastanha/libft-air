cff2216ad36f96f2da9f7d84075ef6e6535b077b
Herbert Castanha <hcastanha@Herberts-MacBook-Air.local>
2020-05-07 01:00:17 -0300

    itoa a norminettar

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,41 +6,38 @@
 /*   By: hcastanh <hcastanh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 13:38:49 by hcastanh          #+#    #+#             */
-/*   Updated: 2020/05/06 18:45:55 by hcastanh         ###   ########.fr       */
+/*   Updated: 2020/05/07 00:56:58 by hcastanh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-	char *s;
-	int i = 0;
-	int n_cpy;
+	char	*s;
+	int		i;
+	int		n_cpy;
 
-	if ((long)n == -2147483648)
-		return((char *)"-2147483648");
+	i = 0;
 	n_cpy = n;
 	while (n_cpy)
 	{
-		n_cpy /= 10;
+		n_cpy = n_cpy / 10;
 		i++;
 	}
-	if (n < 0)
-	{
-		n_cpy = -n;
+	n_cpy = n;
+	if (n <= 0)
 		i++;
-	}
-	else
-		n_cpy = n;
-s = malloc(sizeof(char) * (i + 1));
+	s = (char *)malloc(sizeof(char) * (i + 1));
 	if (s == NULL)
 		return (NULL);
 
 	s[i] = '\0';
 	while (i--)
 	{
-		s[i] = (n_cpy % 10) + '0';
+		s[i] = (n_cpy % 10);
+		s[i] *= ((n_cpy < 0) ? -1 : 1);
+		s[i] += '0';
 		n_cpy /= 10;
 	}
 	if (n < 0)

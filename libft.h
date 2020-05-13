1df0231e00fe5f7e47c1a359438d7b41e922c755
Herbert Castanha <hcastanha@Herberts-MacBook-Air.local>
2020-05-13 10:37:44 -0300

    norming itoa and updating header

--- libft.h
+++ libft.h
@@ -1,15 +1,21 @@
 #ifndef __LIBFT_H__
 # define __LIBFT_H__
 
-#include <unistd.h>
-#include <stdio.h>
-#include <limits.h>
-#include <string.h>
-#include <stdlib.h>
+#include <unistd.h>	
+#include <stdio.h>	//verificar
+#include <limits.h>	
+#include <string.h>	//verificar
+#include <stdlib.h>	//malloc
 
 #ifndef size_t
 # define size_t unsigned long
 #endif
+
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
 int     ft_atoi(const char *str);
 void    ft_bzero(void *s, size_t n);
@@ -24,6 +30,8 @@ size_t  ft_strlen(const char *s);
 size_t	ft_strnlen(const char *s, size_t maxlen);
 size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
+char	*ft_strdup(const char *s1);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 char	*ft_itoa(int n);
 char	*ft_strchr(const char *s, int c);
@@ -46,5 +54,16 @@ void    ft_putchar_fd(char c, int fd);
 void    ft_putendl_fd(char *s, int fd);
 void    ft_putnbr_fd(int n, int fd);
 void    ft_putstr_fd(char *s, int fd);
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
 
 #endif

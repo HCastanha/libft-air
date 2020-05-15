add574b5e565fb6d629070d6a482570f87493999
Herbert Castanha <hcastanha@Herberts-MacBook-Air.local>
2020-05-15 13:20:08 -0300

    ft_splitted normed and functional

--- ft_split.c
+++ ft_split.c
@@ -5,64 +5,92 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hcastanh <hcastanh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/05/13 17:59:22 by hcastanh          #+#    #+#             */
-/*   Updated: 2020/05/15 04:31:28 by hcastanh         ###   ########.fr       */
+/*   Created: 2020/05/14 18:48:28 by pde-carv          #+#    #+#             */
+/*   Updated: 2020/05/15 13:14:30 by hcastanh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-** int	main()
-** {
-** 	char *in = "pedro-de-carv";
-** 	char **out = ft_split(in, '-');
-** 	int i = 0;
-** 	while (*((*out)+i))
-** 	{
-** 		ft_putendl(*((*out)+i));
-** 		i++;
-** 	}
-** }
-*/
 #include "libft.h"
+
+/*
+**	Description
+**	-----------
+**	Parse a string searching for a delimiter and split the string,
+**	in an array containing the found strings.
+**
+**	Parameters
+**	----------
+**	char const *s: the string to split.
+**	char c: the char to be used as delimiter.
+**
+**	Returns
+**	-------
+**	An array of the strings resulted from the split.
+**	NULL in case the allocation fails.
+*/
+
+int		count_words(const char *s, char c)
+{
+	int		i;
+	int		words;
+
+	i = 0;
+	words = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && i == 0)
+			words++;
+		else if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
+			words++;
+		i++;
+	}
+	return (words);
+}
+
+char	*ft_getfields(int *a, const char *s, char c)
+{
+	char	*field;
+	int		j;
+	int		i;
+
+	i = *a;
+	field = NULL;
+	while (s[i] == c)
+		i++;
+	j = i;
+	while (s[i] && s[i] != c)
+		i++;
+	if (!(field = (char *)malloc((i - j + 1) * sizeof(char))))
+		return (NULL);
+	i = j;
+	while (s[i] && s[i] != c)
+	{
+		field[i - j] = s[i];
+		i++;
+	}
+	field[i - j] = '\0';
+	while (s[i] && s[i] == c)
+		i++;
+	return (field);
+}
 
 char	**ft_split(const char *s, char c)
 {
 	int		i;
-	int		j;
 	int		k;
 	char	**split;
 	int		words;
 
-	i = 0;
-	words = 1;
-	while (s[i] != '\0')
-	{
-		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
-			words++;
-		i++;
-	}
+	if (!s)
+		return (NULL);
+	words = count_words(s, c);
 	if (!(split = (char **)malloc((words + 1) * sizeof(char *))))
 		return (NULL);
 	i = 0;
 	k = 0;
 	while (k < words)
 	{
-		while (s[i] == c)
-			i++;
-		j = i;
-		while (s[i] && s[i] != c)
-			i++;
-		if (!(split[k] = (char *)malloc((i - j + 1) * sizeof(char))))
-			return (NULL);
-		i = j;
-		while (s[i] && s[i] != c)
-		{
-			split[k][i - j] = s[i];
-			i++;
-		}
-		split[k][i - j] = '\0';
-		while (s[i] == c)
-			i++;
+		split[k] = ft_getfields(&i, s, c);
 		k++;
 	}
 	split[k] = NULL;
